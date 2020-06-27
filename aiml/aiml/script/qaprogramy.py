da68c00190f1d80f1e7bce6d0d52819914203bcf
nandakishormpai2001 <57388834+nandakishormpai2001@users.noreply.github.com>
2020-06-27 12:03:14 +0530

    issues

--- aiml/aiml/script/qaprogramy.py
+++ aiml/aiml/script/qaprogramy.py
@@ -20,8 +20,7 @@ print("filepath=",filepath,files['aiml'])
 
 my_bot =EmbeddedDataFileBot(files,defaults=True)
 
-questions=["how many Alisha clothes with price between 200 and 1000 and rating is 5",
-   "how many Stamin Mass Gainers of PRICE BETWEEN 100 AND 1000 of Alisha with RATING of 5 are there",
+questions=["get all rating of SWAGGA Women Clogs by dongli thing,WHERE CATEGORY IS pet_supplies_toys",
    "how many Expatriate ladies between age 40 and 44 have Stomach Cancer in year 2011",
    "how many National between age 40 and 44 have Breast Cancer in year 2013",
    "Which is the type of cancer that cause the death of men below age 40",
